0f924aacd9c97db99b7f4f158e322b765f9bb37e
can <canzhao216@gmail.com>
2020-12-25 15:25:13 -0500

    first commit

---
+++ src/main/java/com/zc/repository/UserDao.java
@@ -0,0 +1,15 @@
+package com.zc.repository;
+
+import com.zc.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+    boolean saveUser(User user);
+
+    List<User> fetchAllUser();
+
+    User fetchUserById(Long id);
+
+    boolean deleteUser(Long id);
+}
---
+++ src/main/java/com/zc/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.zc.service;
+
+import com.zc.model.User;
+import com.zc.repository.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@CacheConfig(cacheNames = {"User"})
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public boolean saveUser(User user) {
+        return userDao.saveUser(user);
+    }
+
+    @Override
+    public List<User> fetchAllUser() {
+        return userDao.fetchAllUser();
+    }
+
+    @Override
+    public User fetchUserById(Long id) {
+        return userDao.fetchUserById(id);
+    }
+
+    @Override
+    public boolean deleteUser(Long id) {
+        return userDao.deleteUser(id);
+    }
+}
